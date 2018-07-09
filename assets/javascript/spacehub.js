a8e8a75af146cfd503d2ce15a56274f652516b14
Daemone <kylebehm@gmail.com>
2018-07-08 21:42:37 -0400

    basic_functionality

--- assets/javascript/spacehub.js
+++ assets/javascript/spacehub.js
@@ -8,7 +8,7 @@ var year = d.getFullYear();
 var month = d.getMonth();
 // month names
 var monthNames = ["january", "february", "march", "april", "may", "june",
-  "july", "august", "september", "october", "november", "december"
+    "july", "august", "september", "october", "november", "december"
 ];
 
 //--------Create Functions here--------------------------------------------------------
@@ -19,51 +19,81 @@ var monthNames = ["january", "february", "march", "april", "may", "june",
 var visiblePlanets = {
     // assign a number to each planet
     planetsString: ["mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune"],
-    planetsNumber: [0, 1, 2, 3, 4, 5, 6], 
+    planetsNumber: [0, 1, 2, 3, 4, 5, 6],
     // need an object for years
     year2018: {
         // need a visibility counter for each month + planet
-        january: ["dawn", "null", "morning", "morning", "dawn", "evening", "evening"],
-        ferbruary: ["null", "null", "morning", "morning", "morning", "evening", "dusk"],
-        march: ["dusk", "dusk", "morning", "morning", "morning", "evening", "null"],
-        april: ["dawn", "dusk", "morning", "morning", "morning", "null", "morning"],
-        may: ["dawn", "evening", "morning", "all night", "morning", "dawn", "morning"],
-        june: ["null", "evening", "morning", "evening", "all night", "morning", "morning"],
-        july: ["dusk", "evening", "all night", "evening", "evening", "morning", "morning"],
-        august: ["dawn", "evening", "evening", "evening", "evening", "morning", "morning"],
-        september: ["null", "evening", "evening", "evening", "evening", "morning", "all night"],
-        october: ["dusk", "dusk", "evening", "evening", "evening", "all night", "evening"],
-        november: ["dusk", "dawn", "evening", "null", "evening", "evening", "evening"],
-        december: ["dawn", "morning", "evening", "dawn", "dusk", "evening", "evening"],
-    }, 
+        january: ["at dawn", "null", "in the morning", "in the morning", "at dawn", "in the evening", "in the evening"],
+        february: ["null", "null", "in the morning", "in the morning", "in the morning", "in the evening", "at dusk"],
+        march: ["at dusk", "at dusk", "in the morning", "in the morning", "in the morning", "in the evening", "null"],
+        april: ["at dawn", "at dusk", "in the morning", "in the morning", "in the morning", "null", "in the morning"],
+        may: ["at dawn", "in the evening", "in the morning", "all night", "in the morning", "at dawn", "in the morning"],
+        june: ["null", "in the evening", "in the morning", "in the evening", "all night", "in the morning", "in the morning"],
+        july: ["at dusk", "in the evening", "all night", "in the evening", "in the evening", "in the morning", "in the morning"],
+        august: ["at dawn", "in the evening", "in the evening", "in the evening", "in the evening", "in the morning", "in the morning"],
+        september: ["null", "in the evening", "in the evening", "in the evening", "in the evening", "in the morning", "all night"],
+        october: ["at dusk", "at dusk", "in the evening", "in the evening", "in the evening", "all night", "in the evening"],
+        november: ["at dusk", "at dawn", "in the evening", "null", "in the evening", "in the evening", "in the evening"],
+        december: ["at dawn", "in the morning", "in the evening", "at dawn", "at dusk", "in the evening", "in the evening"],
+    },
     year2019: {
         // visibility counter for each month + planet
-        january: ["null", "morning", "evening", "morning", "null", "evening", "evening"],
-        ferbruary: ["dusk", "morning", "evening", "morning", "morning", "evening", "dusk"],
-        march: ["null", "morning", "evening", "morning", "morning", "evening", "null"],
-        april: ["dawn", "morning", "evening", "morning", "morning", "null", "morning"],
-        may: ["null", "dawn", "evening", "morning", "morning", "dawn", "morning"],
-        june: ["dusk", "dawn", "dusk", "all night", "morning", "morning", "morning"],
-        july: ["null", "null", "dusk", "evening", "evening", "all night", "morning"],
-        august: ["dawn", "null", "null", "evening", "evening", "morning", "morning"],
-        september: ["null", "null", "null", "evening", "evening", "morning", "all night"],
-        october: ["dusk", "dusk", "dawn", "evening", "evening", "all night", "evening"],
-        november: ["dawn", "dusk", "dawn", "evening", "evening", "evening", "evening"],
-        december: ["dawn", "evening", "morning", "null", "dusk", "evening", "evening"],        
+        january: ["null", "in the morning", "in the evening", "in the morning", "null", "in the evening", "in the evening"],
+        february: ["at dusk", "in the morning", "in the evening", "in the morning", "in the morning", "in the evening", "at dusk"],
+        march: ["null", "in the morning", "in the evening", "in the morning", "in the morning", "in the evening", "null"],
+        april: ["at dawn", "in the morning", "in the evening", "in the morning", "in the morning", "null", "in the morning"],
+        may: ["null", "at dawn", "in the evening", "in the morning", "in the morning", "at dawn", "in the morning"],
+        june: ["at dusk", "at dawn", "at dusk", "all night", "in the morning", "in the morning", "in the morning"],
+        july: ["null", "null", "at dusk", "in the evening", "in the evening", "all night", "in the morning"],
+        august: ["at dawn", "null", "null", "in the evening", "in the evening", "in the morning", "in the morning"],
+        september: ["null", "null", "null", "in the evening", "in the evening", "in the morning", "all night"],
+        october: ["at dusk", "at dusk", "at dawn", "in the evening", "in the evening", "all night", "in the evening"],
+        november: ["at dawn", "at dusk", "at dawn", "in the evening", "in the evening", "in the evening", "in the evening"],
+        december: ["at dawn", "in the evening", "in the morning", "null", "at dusk", "in the evening", "in the evening"],
     },
-    displayVisibility: function() {
+    displayVisibility: function () {
         // get month name
         var monthName = monthNames[d.getMonth()];
-        console.log(this.year2018[monthName][2]); 
+        // if year is 2018
+        if (year === 2018) {
+            // print visibility status for each planet
+            for (i = 0; i < this.planetsString.length; i++) {
+                // check the planet visibility status
+                if (this.year2018[monthName][i] !== "null") {
+                    // display visibility stats in html div
+                    $("#visibility").append(this.planetsString[i] + " will be visbile " + this.year2018[monthName][i] + "<br>");
+                }
+                else {
+                    // display nothing if not visible
+                    $("#visibility").append();
+                }
+            }
+        }
+        // if year if 2019
+        else if (year === 2019) {
+            // run function same as before with year2019
+            for (i = 0; i < this.planetsString.length; i++) {
+                // check the planet visibility status
+                if (this.year2019[monthName][i] !== "null") {
+                    // display visibility stats in html div
+                    $("#visibility").append(this.planetsString[i] + " will be visbile " + this.year2018[monthName][i] + "<br>");
+                }
+                else {
+                    // display nothing if not visible
+                    $("#visibility").append();
+                }
+            }
+
+        }
     }
 }
 
 //-------Once the page loads, execute these functions----------------------------------
 $(document).ready(function () {
 
-    {
-        alert("The javascript file is linked!");
-    }
+    // {
+    //     alert("The javascript file is linked!");
+    // }
 
     visiblePlanets.displayVisibility();
 });
